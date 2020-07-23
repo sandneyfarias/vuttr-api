b933d5358dc3c09867db27698a201182ce6f209d
Sandney Farias da Cunha <sandneyfarias@hotmail.com>
2020-07-23 08:05:51 -0300

    Update V1__Create_tool_table.sql
    Removendo a coluna tags da tabela tools, que foi usada em uma versão inicial

--- src/main/resources/db/migration/V1__Create_tool_table.sql
+++ src/main/resources/db/migration/V1__Create_tool_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE "tool"
 	 "tool_id" serial NOT NULL,
 	 "title"          varchar(200) NOT NULL,
 	 "link"           varchar(200) NOT NULL,
-	 "description"     text NOT NULL,
-	 "tags"            text NOT NULL,
+	 "description"     text NOT NULL
 	 CONSTRAINT "PK_ferramenta" PRIMARY KEY ( "tool_id" )
 );
