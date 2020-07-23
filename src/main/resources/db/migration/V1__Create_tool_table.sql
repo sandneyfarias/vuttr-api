6fa79e71c5c70f9df88daec2bae3be242bc9ed66
Sandney Farias da Cunha <sandneyfarias@hotmail.com>
2020-07-23 08:07:11 -0300

    Update V1__Create_tool_table.sql
    Corrigindo o script de criação da tabela tool

--- src/main/resources/db/migration/V1__Create_tool_table.sql
+++ src/main/resources/db/migration/V1__Create_tool_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE "tool"
 	 "tool_id" serial NOT NULL,
 	 "title"          varchar(200) NOT NULL,
 	 "link"           varchar(200) NOT NULL,
-	 "description"     text NOT NULL
+	 "description"     text NOT NULL,
 	 CONSTRAINT "PK_ferramenta" PRIMARY KEY ( "tool_id" )
 );
